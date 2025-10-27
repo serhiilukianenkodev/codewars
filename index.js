4cac53b09f8e655a6f30ba9d8b96fc6f2d9a4224
serhiilukianenkodev <99186322+serhiilukianenkodev@users.noreply.github.com>
2025-10-27 12:11:23 +0200

    kata

--- index.js
+++ index.js
@@ -503,11 +503,23 @@
 
 // patrick feeney => P.F
 
-function abbrevName(name) {
-  return name
-    .split(" ")
-    .map((word) => word[0].toUpperCase())
-    .join(".");
+// function abbrevName(name) {
+//   return name
+//   .split(" ")
+//   .map((word) => word[0].toUpperCase())
+//   .join(".");
+// }
+
+// console.log(abbrevName("Patrick Feenan"));
+
+// **************************************************************************
+
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+// Return your answer as a number.
+
+function sumMix(x) {
+  return x.reduce((sum, i) => sum + Number(i), 0);
 }
 
-console.log(abbrevName("Patrick Feenan"));
+console.log(sumMix(["5", "0", 9, 3, 2, 1, "9", 6, 7]));
