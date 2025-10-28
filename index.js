a7581004cb191f8fb8282746a68da0a897512716
serhiilukianenkodev <99186322+serhiilukianenkodev@users.noreply.github.com>
2025-10-28 22:19:40 +0200

    kata

--- index.js
+++ index.js
@@ -518,8 +518,23 @@
 
 // Return your answer as a number.
 
-function sumMix(x) {
-  return x.reduce((sum, i) => sum + Number(i), 0);
+// function sumMix(x) {
+//   return x.reduce((sum, i) => sum + Number(i), 0);
+// }
+
+// console.log(sumMix(["5", "0", 9, 3, 2, 1, "9", 6, 7]));
+
+// **************************************************************************
+
+// Your function takes two arguments:
+
+// current father's age (years)
+// current age of his son (years)
+// Сalculate how many years ago the father was twice as old as his son(or in how many years he will be twice as old).
+// The answer is always greater or equal to 0, no matter if it was in the past or it is in the future.
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  return Math.abs(dadYearsOld - 2 * sonYearsOld);
 }
 
-console.log(sumMix(["5", "0", 9, 3, 2, 1, "9", 6, 7]));
+console.log(twiceAsOld(55, 30));
